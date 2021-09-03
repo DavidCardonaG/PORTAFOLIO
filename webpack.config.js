da2d6c4f38d83f31d4b9a8d71f01184ce05220c3
DavidCardonaG <softwarewd@outlook.com>
2021-09-03 02:53:02 -0500

    Estructura inicial

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/app/index.js',
+  "@babel/polyfill",
+],
+mode: "development",
+  output: {
+    filename: 'js/app.bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  devServer:{
+      port:5000
+  },
+  
+  module:{
+    rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+        },
+  
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }
+    ]
+ },
+  plugins: [
+      new HTMLWebpackPlugin({template: './src/index.html',
+    minify:  {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/app.bundle.css'
+    }),
+  ]
+
+};
